0cd38f6562f5fd65da37fec699e396992195b2f4
tentacles <tentacles@MBP-tentacles.fritz.box>
2024-04-29 16:03:24 +0200

    init

---
+++ dot_config/nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,139 @@
+local M = {}
+
+M.mason = {
+	ensure_installed = {
+		-- Lua
+		"lua-language-server",
+		"luacheck",
+		"stylua",
+
+		-- SQL
+		"sqls",
+		"sqlfluff",
+		"sql-formatter",
+
+		-- JavaScript/Typescript
+		"typescript-language-server",
+		"prettierd",
+		"eslint_d",
+
+		-- Rust
+		"rust-analyzer",
+		"rustfmt",
+
+		-- File Formats
+		"json-lsp",
+		"jsonlint",
+		"jq",
+		"yaml-language-server",
+		"yamllint",
+		"yamlfmt",
+
+		-- Git
+		"commitlint",
+		"gitlint",
+
+		-- Writing
+		"marksman",
+		"markdownlint",
+		"vale",
+		"write-good",
+		"cspell",
+		"misspell",
+		"proselint",
+
+		-- Shell
+		"bash-language-server",
+		"beautysh",
+		"shfmt",
+		"shellcheck",
+		"shellharden",
+
+		-- Others
+    "tailwindcss-language-server",
+    -- "vscode-css-language-server",
+		"css-lsp",
+		"codespell",
+		"dockerfile-language-server",
+		"dot-language-server",
+		"editorconfig-checker",
+		"html-lsp",
+	},
+  automatic_installation = true,
+}
+
+vim.api.nvim_create_user_command("MasonInstallAll", function()
+  vim.cmd("MasonInstall " .. table.concat(M.mason.ensure_installed, " "))
+end, {})
+
+M.treesitter = {
+  autotag = {
+    enable = true,
+  },
+  highight = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<leader><tab>",
+      node_incremental = "<leader><tab>",
+      node_decremental = "<bs>",
+      scope_incremental = false
+    },
+  },
+	ensure_installed = {
+		"bash",
+		"cmake",
+		"css",
+		"dockerfile",
+		"dot",
+		"git_rebase",
+		"gitattributes",
+		"gitcommit",
+		"gitignore",
+		"html",
+		"http",
+		"javascript",
+		"jq",
+		"json",
+		"json5",
+		"jsonc",
+		"lua",
+		"make",
+		"markdown",
+		"markdown_inline",
+		"nix",
+		"org",
+		"python",
+		"regex",
+		"rust",
+		"sql",
+		"svelte",
+		"sxhkdrc",
+		"todotxt",
+		"toml",
+		"typescript",
+    "svelte",
+    "tsx",
+		"vim",
+		"yaml",
+	},
+}
+
+M.nvimtree = {
+	git = {
+		enable = true,
+  ignore = false,
+	},
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
+}
+
+return M
---
+++ dot_config/nvim/lua/custom/init.lua
@@ -0,0 +1,44 @@
+vim.cmd("autocmd!")
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.wo.number = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.laststatus = 2
+vim.cmdheight = 1
+vim.scrolloff = 10
+vim.opt.expandtab = true
+vim.opt.showcmd = true
+vim.opt.backupskip = '/tmp/*,/private/tmp/*'
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = true
+vim.opt.si = true
+vim.opt.wrap = false
+vim.opt.backspace = 'start,eol,indent'
+vim.opt.path:append { "**" }
+vim.opt.wildignore:append { '*/node_modules/*,*/.history/*' }
+
+vim.opt.relativenumber = true
+
+vim.opt.undofile = true
+
+vim.opt.formatoptions:append { 'r' }
+
+vim.opt.clipboard:append { 'unnamedplus' }
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.termguicolors = true
+
