6cdfb9bec319b14cd0dae6718d18b3d21c747fbb
philosofonusus <my.tentacles.are.in.you@gmail.com>
2024-05-27 19:34:16 +0200

    fix tabufline file tree space

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,16 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'yoru' }
+M.ui = {
+	theme = "yoru",
+	tabufline = {
+		enabled = true,
+		overriden_modules = function(modules)
+			table.insert(modules, modules[1])
+			table.remove(modules, 1)
+		end,
+	},
+}
 M.plugins = "custom.plugins"
 
 return M
