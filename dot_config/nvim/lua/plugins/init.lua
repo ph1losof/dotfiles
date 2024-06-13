bde97614e9f69a1c7d120e18144774c91645cd7a
philosofonusus <my.tentacles.are.in.you@gmail.com>
2024-06-13 16:18:09 +0200

    autopair + autotag fix

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -387,12 +387,22 @@ return {
   {
     "windwp/nvim-ts-autotag",
     event = "InsertEnter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
   },
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
-    opts = {},
+    -- Optional dependency
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+      local cmp = require "cmp"
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
   },
   {
     "pmizio/typescript-tools.nvim",
