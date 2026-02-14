ce5b56bcfd29d1d8897af1e41d4e7ca186085aa0
ph1losof <my.tentacles.are.in.you@gmail.com>
2026-02-14 18:48:46 +0100

    upate

--- dot_config/fish/conf.d/00-tmux-env.fish
+++ dot_config/fish/conf.d/00-tmux-env.fish
@@ -2,9 +2,9 @@
 # Ghostty on macOS inherits $TMUX from parent tmux sessions,
 # which prevents the tmux.fish plugin autostart from working.
 if set -q TMUX
-    set -l ppid (command ps -o ppid= -p $fish_pid 2>/dev/null | string trim)
-    set -l parent (command ps -o comm= -p $ppid 2>/dev/null | string trim)
-    if not string match -q 'tmux*' -- $parent
+    set -l our_tty (command tty 2>/dev/null)
+    set -l tmux_ttys (command tmux list-panes -a -F '#{pane_tty}' 2>/dev/null)
+    if not contains -- $our_tty $tmux_ttys
         set -e TMUX
         set -e TMUX_PANE
         set -e fish_tmux_autostarted
